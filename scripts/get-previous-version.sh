cff24067c9611a3e7c1534488b5bfa309f29f7a3
FARHAD TARAN <farhad86@gmail.com>
2021-05-11 21:59:42 +0100

    add get-previous-version.sh

---
+++ scripts/get-previous-version.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+for ARGUMENT in "$@"
+do
+
+    KEY=$(echo "$ARGUMENT" | cut -f1 -d=)
+    VALUE=$(echo "$ARGUMENT" | cut -f2 -d=)   
+
+    case "$KEY" in
+            HELP)               HELP=${VALUE} ;;
+            FUNCTION)           FUNCTION=${VALUE} ;;     
+            *)   
+    esac    
+
+
+done
+
+if [ "$HELP" != "" ]; then
+  echo "FUNCTION=my-lambda ./get-latest-version.sh"
+  exit 0
+else
+  true
+fi
+
+latest_version=$(aws lambda list-versions-by-function --function-name "${FUNCTION}" \
+  --no-paginate \
+  --query "max_by(Versions, &to_number(to_number(Version) || '0'))" | jq -r ".Version")
+
+echo "$((latest_version-1))"
