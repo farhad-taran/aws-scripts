34d3036b71a15193dc962d342ad91aa1c108a89b
Farhad Taran <7329422+farhad-taran@users.noreply.github.com>
2025-03-13 19:48:52 +0000

    Update traffic-shifting.sh

--- lambda/traffic-shifting.sh
+++ lambda/traffic-shifting.sh
@@ -1,3 +1,4 @@
+            
 #!/bin/sh
 for ARGUMENT in "$@"
 do
@@ -21,25 +22,37 @@ if [ $PERCENTAGE -lt "0" ]; then
     PERCENTAGE=0
 fi
 
-SCRIPT_PATH="$( cd "${SCRIPT_INVOKED_PATH}"; pwd )"
 MAX_PERCENTAGE=100
 GRACE_MINUTES_CM=${GRACE_MINUTES}m
-LATEST_VERSION=$(.$SCRIPT_PATH/get-latest-version.sh FUNCTION=$FUNCTION REGION=$REGION)
+
+LATEST_VERSION=$(aws lambda --region "$REGION" list-versions-by-function --function-name "${FUNCTION}" \
+  --no-paginate \
+  --query "max_by(Versions, &to_number(to_number(Version) || '0'))" | jq -r ".Version")
 PREVIOUS_VERSION=$((LATEST_VERSION-1))
+
 echo "lambda latest version: $LATEST_VERSION"
 echo "lambda previous version: $PREVIOUS_VERSION"
+
 while [[ $PERCENTAGE -lt $MAX_PERCENTAGE ]]
 do
     echo "waiting for grace period: ${GRACE_MINUTES_CM}"
+    
     sleep $GRACE_MINUTES_CM
+    
     PERCENTAGE=$(($PERCENTAGE * 2))
+    
     if [ $PERCENTAGE -gt "100" ]; then
         PERCENTAGE=100
     fi
+    
     LATEST_VERSION_PERCENTAGE=$PERCENTAGE
+
     # in order for traffic shifting to work, the percentage is set on the previous version and not the latest version
     PREVIOUS_VERSION_PERCENTAGE=$((100-$LATEST_VERSION_PERCENTAGE))
+    
     echo "setting latest version: $LATEST_VERSION traffic to $LATEST_VERSION_PERCENTAGE %"
     echo "setting previous version: $PREVIOUS_VERSION traffic to $PREVIOUS_VERSION_PERCENTAGE %"
-    .$SCRIPT_PATH/split-traffic.sh FUNCTION=$FUNCTION ALIAS=LATEST VERSION=$PREVIOUS_VERSION PERCENTAGE=$PREVIOUS_VERSION_PERCENTAGE REGION=$REGION
+    
+    PERCENTAGE_DECIMAL=$(awk "BEGIN {printf \"%.1f\",$PERCENTAGE/100}")
+    aws lambda update-alias --region "$REGION" --name "$ALIAS" --function-name "$FUNCTION" --routing-config AdditionalVersionWeights="{${VERSION}=${PERCENTAGE_DECIMAL}}"
 done
